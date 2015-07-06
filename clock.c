360a8c1dd424bc9191109e088716310cc755e924
Aditya Vaidya <kroq.gar78@gmail.com>
2015-07-06 11:25:48 -0500

    Added C and FORTRAN files for csparse and expokit (dschbv)

---
+++ clock.c
@@ -0,0 +1,30 @@
+/*  -- translated by f2c (version 20100827).
+   You must link the resulting object file with libf2c:
+	on Microsoft Windows system, link with libf2c.lib;
+	on Linux or Unix systems, link with .../path/to/libf2c.a -lm
+	or, if you install libf2c.a in a standard place, with -lf2c -lm
+	-- in that order, at the end of the command line, as in
+		cc *.o -lf2c -lm
+	Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
+
+		http://www.netlib.org/f2c/libf2c.zip
+*/
+
+#include "f2c.h"
+
+/* ---  Utility timer routine (seconds). */
+/* ---  uncomment the appropriate one and comment the others */
+/* ---  SUN & SGI -- */
+doublereal clock_(void)
+{
+    /* System generated locals */
+    doublereal ret_val;
+
+    /* Local variables */
+    static real tm[2];
+    extern doublereal etime_(real *);
+
+    ret_val = etime_(tm);
+    return ret_val;
+} /* clock_ */
+
